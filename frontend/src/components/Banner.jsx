80d37bcef8944fbdfe81a029e4e6b1ea1392ef55
unknown <sakshibandichawade@gmail.com>
2025-09-04 11:30:42 +0530

    updated home page banners

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -5,6 +5,7 @@ export default function Banner({ onSubscribe }) {
       <img
         src="/Tapbanner.webp"
         alt="Craft culture banner"
+        onClick={onSubscribe}
         className="hidden md:block h-[100vh] w-full object-cover center-center"
       />
 
@@ -12,24 +13,11 @@ export default function Banner({ onSubscribe }) {
       <img
         src="/Mobiletapbanner.webp"
         alt="Craft culture banner mobile"
+        onClick={onSubscribe}
         className="block md:hidden h-[100vh] w-full object-fit center-center"
       />
 
-      <div className="absolute" />
-      <div className="absolute bottom-[11.5%] right-[25%] md:bottom-[15%] md:right-[10%] flex justify-end text-white">
-        <img
-          src="/subscribenow.png"
-          alt="Subscribe Now"
-          onClick={onSubscribe}
-          className="hidden md:block cursor-pointer w-[50%] h-auto"
-        />
-        <img
-          src="/Mobilesubscribebtn.png"
-          alt="Subscribe Now"
-          onClick={onSubscribe}
-          className="md:hidden cursor-pointer w-[180px] h-auto"
-        />
-      </div>
+     
     </header>
   );
 }
